50d526e21435bec33bf704db954978194ef70a55
slightlycyborg <collinalexbell@gmail.com>
2018-05-11 06:39:48 -0400

    alex.lisp added

---
+++ alex.lisp
@@ -0,0 +1,11 @@
+
+
+(defparameter *daemon-name* "Alexander Taggart")
+(defparameter *human-name* "Collin Alexander Bell")
+
+(defun DescribeAlex ()
+  (princ *human-name*)
+  (princ " is ")
+  (princ (ash (+ 16 36) -1))
+  (format t " years of age.~%")
+  (format t "He is building a daemon called ~s" *daemon-name*))
